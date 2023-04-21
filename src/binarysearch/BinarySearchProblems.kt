e48ec90bc918239e511f0f7e0c95fd7b37ca0a4e
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2023-04-21 21:24:56 +0530

    added binary search qs

--- src/binarysearch/BinarySearchProblems.kt
+++ src/binarysearch/BinarySearchProblems.kt
@@ -1,4 +1,74 @@
 package binarysearch
 
 interface BinarySearchProblems {
+
+    // [10, 20, 30, 50, 60, 80, 110, 130, 140, 170]
+    // 110 -> 6
+    fun binarySearch(items: List<Int> = listOf(10, 20, 30, 50, 60, 80, 110, 130, 140, 170), item: Int = 110): Int
+
+    fun reverseBinarySearch(items: List<Int>, item: Int): Int
+
+    // [1, 3, 5, 5, 5, 5, 67, 123, 125]
+    // 5 -> 2
+    fun firstOccurrenceOfElement(items: List<Int> = listOf(1, 3, 5, 5, 5, 5, 67, 123, 125), item: Int = 5): Int
+
+    // [1, 3, 5, 5, 5, 5, 67, 123, 125]
+    // 5 -> 5
+    fun lastOccurrenceOfElement(items: List<Int> = listOf(1, 3, 5, 5, 5, 5, 67, 123, 125), item: Int = 5): Int
+
+    // [1, 3, 5, 5, 5, 5, 67, 123, 125]
+    // 5 -> 4
+    fun countOfAnElement(items: List<Int> = listOf(1, 3, 5, 5, 5, 5, 67, 123, 125), item: Int = 5): Int
+
+    // [1, 2, 8, 10, 10, 12, 19]
+    // 5 -> 2
+    fun floorOfAnElement(items: List<Int> = listOf(1, 2, 8, 10, 10, 12, 19), item: Int = 5): Int
+
+    // [1, 2, 8, 10, 10, 12, 19]
+    // 5 -> 8
+    fun cielOfAnElement(items: List<Int> = listOf(1, 2, 8, 10, 10, 12, 19), item: Int = 5): Int
+
+    // [2, 5, 10, 12, 15]
+    // 5 -> 5
+    fun minimumDifferenceElement(items: List<Int> = listOf(2, 5, 10, 12, 15), item: Int = 5): Int
+
+    fun nextAlphabetInList(items: List<Char> = listOf('D', 'J', 'K', 'L'), item: Char = 'D'): Char
+
+    // [15, 18, 2, 3, 6, 12] -> 2
+    fun numberOfRotation(items: List<Int> = listOf(15, 18, 2, 3, 6, 12)): Int
+
+    // [15, 18, 2, 3, 6, 12]
+    // 6 -> 4
+    fun binarySearchInRotatedList(items: List<Int> = listOf(15, 18, 2, 3, 6, 12), item: Int = 6): Int
+
+    // [5, 10, 20, 15]
+    fun peakElement(items: List<Int> = listOf(5, 10, 20, 15)): Int
+
+    fun firstOccurrenceOfOne(items: List<Int> = listOf(0, 0, 0, 0, 0, 0, 0, 1, 1)): Int
+
+    fun maximumElementInBitonicList(items: List<Int> = listOf(100, 80, 60, 40, 20, 0)): Int
+
+    fun binarySearchInBitonicList(items: List<Int> = listOf(3, 23, 10, 8, 7, 6), item: Int = 23): Int
+
+    fun binarySearchInInfiniteList(
+        items: List<Int> = listOf(10, 20, 30, 50, 60, 80, 110, 130, 140, 170),
+        item: Int = 110
+    ): Int
+
+    fun binarySearchInNearlySortedList(items: List<Int> = listOf(10, 3, 40, 20, 50, 80, 70), item: Int = 50): Int
+
+    fun binarySearchInUnknownOrder(
+        items: List<Int> = listOf(10, 20, 30, 50, 60, 80, 110, 130, 140, 170),
+        item: Int = 110
+    ): Int
+
+    fun binarySearchInMatrix(
+        items: List<List<Int>> = listOf(
+            listOf(10, 20, 30, 40),
+            listOf(15, 25, 35, 45),
+            listOf(27, 29, 37, 48),
+            listOf(32, 33, 39, 50)
+        ), item: Int = 29
+    ): Pair<Int, Int>
+
 }
--- src/binarysearch/BinarySearchSolutions.kt
+++ src/binarysearch/BinarySearchSolutions.kt
@@ -1,4 +1,79 @@
 package binarysearch
 
 class BinarySearchSolutions : BinarySearchProblems {
+    override fun binarySearch(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun reverseBinarySearch(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun firstOccurrenceOfElement(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun lastOccurrenceOfElement(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun countOfAnElement(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun floorOfAnElement(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun cielOfAnElement(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun minimumDifferenceElement(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun nextAlphabetInList(items: List<Char>, item: Char): Char {
+        TODO("Not yet implemented")
+    }
+
+    override fun numberOfRotation(items: List<Int>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun binarySearchInRotatedList(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun peakElement(items: List<Int>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun firstOccurrenceOfOne(items: List<Int>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun maximumElementInBitonicList(items: List<Int>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun binarySearchInBitonicList(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun binarySearchInInfiniteList(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun binarySearchInNearlySortedList(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun binarySearchInUnknownOrder(items: List<Int>, item: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun binarySearchInMatrix(items: List<List<Int>>, item: Int): Pair<Int, Int> {
+        TODO("Not yet implemented")
+    }
 }
