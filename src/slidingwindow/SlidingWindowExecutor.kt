f1c4350ee6fdcd894d57580b6bafea71e37caac9
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2023-04-29 15:57:08 +0530

    added sliding window ans

--- src/slidingwindow/SlidingWindowExecutor.kt
+++ src/slidingwindow/SlidingWindowExecutor.kt
@@ -3,14 +3,33 @@ package slidingwindow
 object SlidingWindowExecutor : SlidingWindowProblems by SlidingWindowSolutions() {
 
     operator fun invoke() {
+        println("\nSlidingWindow :\n")
+
+        print("countOfOccurrencesOfAnagram : ")
         countOfOccurrencesOfAnagram()
+
+        print("firstNegativeNumberInWindow : ")
         firstNegativeNumberInWindow()
+
+        print("longestSubArrayOfSumK : ")
         longestSubArrayOfSumK()
+
+        print("longestSubStringWithKUniqueChar : ")
         longestSubStringWithKUniqueChar()
+
+        print("longestSubStringWithoutRepeatingChar : ")
         longestSubStringWithoutRepeatingChar()
+
+        print("maximumElementOfAllSubArray : ")
         maximumElementOfAllSubArray()
+
+        print("maximumSumOfArrayOfSizeK : ")
         maximumSumOfArrayOfSizeK()
+
+        print("minimumWindowSubString : ")
         minimumWindowSubString()
+
+        print("pickToys : ")
         pickToys()
     }
 }
